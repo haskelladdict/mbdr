5ecadc94957b8f9123eb953ba3242478eab1f039
Markus Dittrich <haskelladdict@gmail.com>
2014-10-01 17:38:22 -0400

    Added code for parsing data blocks
    from the underlying binary data stream. Also added initial code
    for selecting and printingout selected datasets.

--- libmbd/libmbd.go
+++ libmbd/libmbd.go
@@ -18,22 +18,18 @@ const (
 	len_double   = 8
 )
 
-// enumerations defining the output type of the included data
+// enumeration describing the output type of the included data
 const (
 	Step uint16 = 1 << iota
 	TimeList
 	IterationList
 )
 
-// blockData stores metadata related to a given data block such as the
-// data name, number of data columns, the type of data stored (int/double),
-// and the internal offset into the buffer at which the data can be found.
-type blockData struct {
-	name      string
-	numCols   uint64
-	dataTypes []uint16
-	offset    uint64
-}
+// enumeration describing the data type of a data column
+const (
+	IntData = iota
+	DoubleData
+)
 
 // MCellData tracks the data contained in the binary mcell file as well as
 // relevant metadate to retrieve specific data items.
@@ -44,10 +40,28 @@ type MCellData struct {
 	stepSize      float64
 	timeList      []float64
 	outputBufSize uint64
+	totalNumCols  uint64
 	numBlocks     uint64
 	blockNames    []string
 	blockNameMap  map[string]uint64
 	blockInfo     []blockData
+}
+
+// CountData is a container holding the data corresponding to a reaction data
+// output block consisting of a number of columns
+type CountData struct {
+	Col       [][]float64
+	DataTypes []uint16
+}
+
+// blockData stores metadata related to a given data block such as the
+// data name, number of data columns, the type of data stored (int/double),
+// and the internal offset into the buffer at which the data can be found.
+type blockData struct {
+	name      string
+	numCols   uint64
+	dataTypes []uint16
+	offset    uint64
 }
 
 // BlockNames returns the list of available blocknames
@@ -74,6 +88,67 @@ func (d *MCellData) OutputType() uint16 {
 // meaningful is outputType == Step, otherwise this function returns 0
 func (d *MCellData) StepSize() float64 {
 	return d.stepSize
+}
+
+// BlockDataByName returns the data stored in the data block of the given name
+// as a CountData struct
+func (d *MCellData) BlockDataByName(name string) (*CountData, error) {
+	id, ok := d.blockNameMap[name]
+	if !ok {
+		return nil, fmt.Errorf("dataset %s not found", name)
+	}
+
+	return d.BlockDataByID(id)
+}
+
+// BlockDataByID returns the data stored in the data block of the given ID
+// as a CountData struct
+func (d *MCellData) BlockDataByID(id uint64) (*CountData, error) {
+	if id < 0 || id >= d.numBlocks {
+		return nil, fmt.Errorf("supplied data ID %d is out of range", id)
+	}
+
+	entry := d.blockInfo[id]
+	output := &CountData{}
+	output.Col = make([][]float64, entry.numCols)
+	for i := uint64(0); i < entry.numCols; i++ {
+		output.Col[i] = make([]float64, 0, d.blockSize)
+		output.DataTypes = append(output.DataTypes, entry.dataTypes[i])
+	}
+
+	row := uint64(0)
+	stream := uint64(0)
+	bufLoc := uint64(0)
+	// read all rows until we hit the total blockSize
+	for row < d.blockSize {
+
+		// forward to the next stream block if we're done parsing the current one
+		if row > stream*d.outputBufSize {
+			offset := d.outputBufSize
+			// last partial block of length <= d.outputBufSize requires special treatment
+			if row-d.blockSize < d.outputBufSize {
+				offset = d.blockSize - row
+			}
+			// forward to beginning of stream block
+			bufLoc = stream * d.outputBufSize * d.totalNumCols * len_double
+
+			// forward to location within stream block
+			bufLoc += offset * entry.offset * len_double
+
+			stream++
+		}
+
+		// read current row
+		for i := uint64(0); i < entry.numCols; i++ {
+			loc := (d.buffer)[bufLoc:]
+			val := loc.float64NoSlice() //d.buffer[bufLoc:].float64NoSlice()
+			output.Col[i] = append(output.Col[i], val)
+			bufLoc += len_double
+		}
+		row++
+	}
+
+	return output, nil
 }
 
 // Read opens the binary mcell data file and attempts to parse its content
@@ -150,11 +225,11 @@ func parseBlockNames(data *MCellData) error {
 	// initialize blockname map
 	data.blockNameMap = make(map[string]uint64)
 
-	var offset uint64
+	var totalCols uint64
 	for count := uint64(0); count < data.numBlocks; count++ {
 
 		e := blockData{}
-		e.offset = offset
+		e.offset = totalCols
 		var i int
 
 		// find end of next block name
@@ -167,12 +242,13 @@ func parseBlockNames(data *MCellData) error {
 		data.buffer = data.buffer[i+1:]
 
 		e.numCols = data.buffer.uint64()
-		offset += e.numCols
+		totalCols += e.numCols
 		for c := uint64(0); c < e.numCols; c++ {
 			e.dataTypes = append(e.dataTypes, data.buffer.uint16())
 		}
 		data.blockInfo = append(data.blockInfo, e)
 	}
+	data.totalNumCols = totalCols
 
 	return nil
 }
@@ -224,5 +300,10 @@ func (b *readBuf) uint64() uint64 {
 func (b *readBuf) float64() float64 {
 	v := math.Float64frombits(binary.LittleEndian.Uint64(*b))
 	*b = (*b)[8:]
+	return v
+}
+
+func (b *readBuf) float64NoSlice() float64 {
+	v := math.Float64frombits(binary.LittleEndian.Uint64(*b))
 	return v
 }
--- mbdr.go
+++ mbdr.go
@@ -12,9 +12,13 @@ const mbdrVersion = 2
 
 // command line flags
 var infoFlag bool
+var listFlag bool
+var extractIDFlag int
 
 func init() {
 	flag.BoolVar(&infoFlag, "i", false, "show general info")
+	flag.BoolVar(&listFlag, "l", false, "list available data blocks")
+	flag.IntVar(&extractIDFlag, "I", -1, "extract dataset at given index")
 }
 
 // main function entry point
@@ -34,12 +38,13 @@ func main() {
 	switch {
 	case infoFlag:
 		showInfo(data)
+
+	case listFlag:
+		showAvailableData(data)
+
+	case extractIDFlag >= 0:
+		showDataByID(data, extractIDFlag)
 	}
-	/*
-		for i, s := range data.GetBlockNames() {
-			fmt.Println(i, s)
-		}
-	*/
 }
 
 // usage prints a brief usage information to stdout
@@ -68,5 +73,32 @@ func showInfo(d *libmbd.MCellData) {
 
 	default:
 		fmt.Printf("mbdr> encountered UNKNOWN output type")
+	}
+}
+
+// showAvailableData shows the available data sets contained in the
+// binary output file
+func showAvailableData(d *libmbd.MCellData) {
+	for i, n := range d.BlockNames() {
+		fmt.Printf("[%d] %s\n", i, n)
+	}
+}
+
+// showDataByID prints the content of the data set at the requested ID
+// NOTE: This routine doesn't bother with converting integer column data and
+// simply prints it as double
+func showDataByID(d *libmbd.MCellData, id int) {
+	data, err := d.BlockDataByID(uint64(id))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	numCols := len(data.Col)
+	numRows := len(data.Col[0])
+	for r := 0; r < numRows; r++ {
+		for c := 0; c < numCols; c++ {
+			fmt.Printf("%g", data.Col[c][r])
+		}
+		fmt.Printf("\n")
 	}
 }
